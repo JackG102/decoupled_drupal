dbb7b9349b1dc2fe40c0547bbb3524047879d7c9
Jack Garratt <jack.garratt@icfnext.com>
2022-03-18 15:37:51 -0400

    Added React USWDS CSS import statements of root of frontend react project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Alert } from '@trussworks/react-uswds';
 
 function App() {
   return (
     <div className="App">
       <h1>Decoupled Drupal React USWDS Project</h1>
+      <Alert type="info" heading="Informative status">
+        Here is some text for folks to use
+      </Alert>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '@trussworks/react-uswds/lib/uswds.css'
+import '@trussworks/react-uswds/lib/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));

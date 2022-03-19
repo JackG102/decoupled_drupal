b55fb754b09a179dccc52d49c9fe5653647cab35
Jack Garratt <jack.garratt@icfnext.com>
2022-03-19 09:36:12 -0400

    Removed USWDS for now to simplify project for me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Alert } from '@trussworks/react-uswds';
 
 function App() {
   return (
     <div className="App">
       <h1>Decoupled Drupal React USWDS Project</h1>
-      <Alert type="info" heading="Informative status">
-        Here is some text for folks to use
-      </Alert>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '@trussworks/react-uswds/lib/uswds.css'
-import '@trussworks/react-uswds/lib/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));

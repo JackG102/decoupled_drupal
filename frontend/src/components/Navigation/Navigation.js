64bb188b61cc69acb335bd430e662550b2e91138
Jack Garratt <jack.garratt@icfnext.com>
2022-03-19 10:56:28 -0400

    Created custom hook to make fetch requests -- tbh I found a great code snippet

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import useApiRequest from '../../hooks/useApiRequest';
 
 const Navigation = () => {
+
+  const { data, error, isLoaded } = useApiRequest(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+
   return (
     <nav>
       Navigation component

fad8c2ed34fcb39e723729e1982460788cdde23d
Jack Garratt <jack.garratt@icfnext.com>
2022-03-20 09:20:14 -0400

    Continuing with Resource template building

--- frontend/src/content_types/Resource/Resource.js
+++ frontend/src/content_types/Resource/Resource.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import useApiRequest from '../../hooks/useApiRequest';
 
 const Resource = () => {
+
+  const { error, isLoaded, data } = useApiRequest('http://backend.docksal.site/jsonapi/node/resource/191eb35d-0b03-428e-9ef4-dbc9ca2f8cb7');
+
+  if (!isLoaded) {
+    return <span>Loading...</span>
+  };
+
   return (
     <div>
       <h3>Resource</h3>

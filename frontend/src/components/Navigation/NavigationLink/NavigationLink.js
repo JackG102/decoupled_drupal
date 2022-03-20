b716f713e1dda64f9e9b3b58971e0f93f9a30d44
Jack Garratt <jack.garratt@icfnext.com>
2022-03-20 08:43:51 -0400

    Fixed href typo in navigation

--- frontend/src/components/Navigation/NavigationLink/NavigationLink.js
+++ frontend/src/components/Navigation/NavigationLink/NavigationLink.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavigationLink = ({props}) => {
   return (
-    <a className="item" href={props.attributes.link}>{props.attributes.title}</a>
+    <a className="item" href={props.attributes.url}>{props.attributes.title}</a>
   );
 }
 

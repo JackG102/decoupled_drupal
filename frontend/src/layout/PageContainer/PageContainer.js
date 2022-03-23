b31902377cc8bdb4fe59162a1a350a0855e04a63
Jack Garratt <jack.garratt@icfnext.com>
2022-03-23 17:58:55 -0400

    Remove unused component in import statement

--- frontend/src/layout/PageContainer/PageContainer.js
+++ frontend/src/layout/PageContainer/PageContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Resource from '../../content_types/Resource/Resource';
 
 const PageContainer = () => {

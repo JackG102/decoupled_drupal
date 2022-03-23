afc2051037ba805d4282919c8190e946fc61ee85
Jack Garratt <jack.garratt@icfnext.com>
2022-03-23 13:25:29 -0400

    Added render for resource type

--- frontend/src/content_types/Resource/Resource.js
+++ frontend/src/content_types/Resource/Resource.js
@@ -3,10 +3,18 @@ import useApiRequest from '../../hooks/useApiRequest';
 
 const Resource = () => {
 
-  const { error, isLoaded, data } = useApiRequest('http://backend.docksal.site/jsonapi/node/resource/191eb35d-0b03-428e-9ef4-dbc9ca2f8cb7?include=field_topics');
+  const { error, isLoaded, data } = useApiRequest('http://backend.docksal.site/jsonapi/node/resource/191eb35d-0b03-428e-9ef4-dbc9ca2f8cb7?include=field_topics,field_resource_type');
 
   const renderTopics = data?.included?.map((el) => {
     if (el.type === "taxonomy_term--topics") {
+      return (
+        <li>{el.attributes.name}</li>
+      );
+    }
+  });
+
+  const renderResourceType = data?.included?.map((el) => {
+    if (el.type === "taxonomy_term--resource_type") {
       return (
         <li>{el.attributes.name}</li>
       );
@@ -28,6 +36,13 @@ const Resource = () => {
           {renderTopics}
         </ul>
       </div>
+      <div>
+        <span>Topics:</span>
+        <ul>
+          {renderResourceType}
+        </ul>
+      </div>
+      <div>Publication Year: {data?.data?.attributes?.field_publicati}</div>
       <div className="resource--body" dangerouslySetInnerHTML={{ __html: data?.data?.attributes?.body.processed }}>
       </div>
     </article>

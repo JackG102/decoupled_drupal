65027c24c40e24c203bc5ef861880a249f3becd2
Jack Garratt <jack.garratt@icfnext.com>
2022-03-23 13:10:43 -0400

    Rendering Topics taxonomy on resource template

--- frontend/src/content_types/Resource/Resource.js
+++ frontend/src/content_types/Resource/Resource.js
@@ -5,15 +5,29 @@ const Resource = () => {
 
   const { error, isLoaded, data } = useApiRequest('http://backend.docksal.site/jsonapi/node/resource/191eb35d-0b03-428e-9ef4-dbc9ca2f8cb7?include=field_topics');
 
+  const renderTopics = data?.included?.map((el) => {
+    if (el.type === "taxonomy_term--topics") {
+      return (
+        <li>{el.attributes.name}</li>
+      );
+    }
+  });
+
   if (!isLoaded) {
     return <span>Loading...</span>
   };
-  console.log(data);
+
   return (
     <article>
       <h3>{data?.data?.attributes?.title}</h3>
-      <span>Resource Author: {data?.data?.attributes?.field_resource_author}</span>
-      <span>Publication Year: {data?.data?.attributes?.field_publicati}</span>
+      <div>Resource Author: {data?.data?.attributes?.field_resource_author}</div>
+      <div>Publication Year: {data?.data?.attributes?.field_publicati}</div>
+      <div>
+        <span>Topics:</span>
+        <ul>
+          {renderTopics}
+        </ul>
+      </div>
       <div className="resource--body" dangerouslySetInnerHTML={{ __html: data?.data?.attributes?.body.processed }}>
       </div>
     </article>
